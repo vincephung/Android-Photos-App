1f99b908510eae92a8221a241de9fee68eb9b204
Vincent Phung <vincentphung43@yahoo.com>
2021-04-23 19:29:13 -0400

    setup some things

--- app/src/main/java/com/example/android50/UserAlbums.java
+++ app/src/main/java/com/example/android50/UserAlbums.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class android50 extends AppCompatActivity {
+public class UserAlbums extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_albums);
     }
 }
